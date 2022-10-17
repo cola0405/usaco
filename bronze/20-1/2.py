2bc4d6a3dc6cedfe459f8a1f632c394d17221a09
cola0405 <1020151695@qq.com>
2022-10-17 17:33:38 +0800

    up

--- bronze/20-1/2.py
+++ bronze/20-1/2.py
@@ -1,22 +1,20 @@
 import sys
-
 sys.stdin = open('photo.in', 'r')
 sys.stdout = open('photo.out', 'w')
-
 n = int(input())
 b = list(map(int, input().split()))
 
-
+# 枚举数字
+# 牛的编号是1到n，不重复
 for i in range(1, n+1):
     j = 0
-    ans = [b[j]]
-    temp_a = b[j] - i
-    if temp_a < 0:
-        continue
-    ans.append(str(temp_a))
+    ans = [str(i)]
+    temp_a = i
     while temp_a > 0 and j < len(b):
         temp_a = b[j] - temp_a
+        ans.append(str(temp_a))
         j += 1
-    if j == len(b):
-        print(''.join(ans))
+    s = set(ans)
+    if len(s) == len(ans) and j == len(b):
+        print(' '.join(ans))
 
