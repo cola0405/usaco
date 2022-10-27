c2aea0b1301bc22863cae0f302484d0e1f29b911
cola0405 <1020151695@qq.com>
2022-10-27 12:04:06 +0800

    bad milk

--- bronze/15-12/3-p100.py
+++ bronze/15-12/3-p100.py
@@ -1,6 +1,15 @@
-# 60%
-# person 2 didn't drink milk 4 but still got sick
-# so the bad milk is 1 or 2, not 1 2 4
+# exactly one of them has gone bad
+# 只有一瓶奶出问题了
+# 但注意，我们不一定能准确地把那一瓶奶找出来
+# 尽可能排除，剩下地都当作有毒的
+
+# 排除的方法：
+# 把每个生病的人喝的可能有毒的奶找出来
+# 有毒的那一瓶一定在小集合里
+# 小集合外的一定是好牛奶
+# 所以利用n个小集合与大集合做差，尽可能排除
+
+
 
 PERSON = 0
 TYPE = 1
@@ -12,6 +21,7 @@ sys.stdin = open('badmilk.in', 'r')
 sys.stdout = open('badmilk.out', 'w')
 n, m, d, s = map(int, input().split())
 
+
 person_type_time = []
 for i in range(d):
     person_type_time.append(list(map(int, input().split())))
@@ -20,6 +30,10 @@ person_sickTime = []
 for i in range(s):
     person_sickTime.append(list(map(int, input().split())))
 
+d = {}
+for i in range(1, n+1):
+    d[i] = set()
+
 # avoid over count
 possible_bad_milk = set()
 for s in person_sickTime:
@@ -27,11 +41,20 @@ for s in person_sickTime:
         if record[PERSON] == s[PERSON] \
                 and record[TIME] < s[SICK_TIME]:
             possible_bad_milk.add(record[TYPE])
+            d[record[PERSON]].add(record[TYPE])
+
+
+for i in d.keys():
+    if len(d[i]) > 0:
+        good_milk = possible_bad_milk.difference(d[i])
+        for t in good_milk:
+            possible_bad_milk.remove(t)
 
 possible_sick_person = set()
 for record in person_type_time:
     if record[TYPE] in possible_bad_milk:
         possible_sick_person.add(record[PERSON])
+
 
 print(len(possible_sick_person))
 
