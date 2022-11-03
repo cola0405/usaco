5d628d3f81c30c4693548161735fc69d30278f98
cola0405 <1020151695@qq.com>
2022-11-03 11:47:01 +0800

    21-1-3

---
+++ bronze/21-1/3-bao.py
@@ -0,0 +1,19 @@
+import itertools
+
+n = int(input())
+a = [int(i) for i in input().split(" ")]
+b = [int(i) for i in input().split(" ")]
+
+c = tuple(itertools.permutations(a,n))
+
+count = 0
+
+for cn in c:
+    flag = True
+    for i in range(n):
+        if cn[i]>b[i]:
+            flag = False
+    if flag:
+        count += 1
+
+print(count)
