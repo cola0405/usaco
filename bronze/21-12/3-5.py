eaad0ca26cceb9d2a808de43ad6b14f644ef5791
cola0405 <1020151695@qq.com>
2022-11-07 12:07:05 +0800

    19-1-1 update

---
+++ bronze/21-12/3-5.py
@@ -0,0 +1,73 @@
+BLOCKED = False
+UNBLOCKED = True
+
+
+def checkRow(path, row, start, end):
+    current_col = start
+    while current_col < end:
+        if path[row][current_col] == 'H':
+            return BLOCKED
+        current_col += 1
+    else:
+        return UNBLOCKED
+
+
+def checkColumn(path, column, start, end):
+    current_row = start
+    while current_row < end:
+        if path[current_row][column] == 'H':
+            return BLOCKED
+        current_row += 1
+    else:
+        return UNBLOCKED
+
+
+round = int(input())
+for r in range(round):
+    n, k = map(int, input().split(" "))
+    path = []
+    for row in range(n):
+        path.append(input())
+    ans1 = 0
+    home = n - 1
+    if k >= 1:
+        # go right at first
+        if checkRow(path, 0, 0, home) \
+                and checkColumn(path, n - 1, 0, home):
+            ans1 += 1
+        # go down at first
+        if checkColumn(path, 0, 0, n - 1) \
+                and checkRow(path, n - 1, 1, home):
+            ans1 += 1
+    ans2 = 0
+    if k >= 2:
+        # go right at first
+        for current_col in range(1, n - 1):
+            if checkColumn(path, current_col, 0, home) \
+                    and checkRow(path, n - 1, current_col + 1, home):
+                ans2 += 1
+        # go down at first
+        for current_row in range(1, n - 1):
+            if checkRow(path, current_row, 0, home) \
+                    and checkColumn(path, n-1, current_row + 1, home):
+                ans2 += 1
+    ans3 = 0
+    if k >= 3:
+        # go right at first
+        for i in range(1, n-1):
+            for j in range(1, n-1):
+                if checkColumn(path, i, 0, j)\
+                    and checkRow(path, j, i+1, n-1)\
+                    and checkColumn(path, n-1, j+1, n-1):
+                    ans3 += 1
+
+        # go down at first
+        for i in range(1, n - 1):
+            for j in range(1, n - 1):
+                if checkRow(path, i, 0, j) \
+                        and checkColumn(path, i, i + 1, n - 1) \
+                        and checkRow(path, n-1, j + 1, n - 1):
+                    ans3 += 1
+    ans = ans1+ans2+ans3
+    print(ans1,ans2,ans3)
+    print(ans)
