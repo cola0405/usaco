67b84a3d463d42b73a826421ea14d2d21b318481
cola0405 <1020151695@qq.com>
2022-11-13 00:39:43 +0800

    17-1-1

---
+++ bronze/17-1/1.py
@@ -0,0 +1,48 @@
+import sys
+sys.stdin = open("notlast.in", 'r')
+sys.stdout = open("notlast.out", 'w')
+
+log = {}
+names = ['Bessie', 'Elsie', 'Daisy',
+         'Gertie', 'Annabelle', 'Maggie', 'Henrietta']
+for i in names:
+    log[i] = 0
+
+n = int(input())
+for i in range(n):
+    name, amount = input().split()
+    log[name] += int(amount)
+
+
+def find_sec():
+    milks = list(log.values())
+    milks.sort()
+
+    # no second
+    M = milks[0]
+    sec = None
+    for i in milks:
+        if i != M:
+            sec = i
+            break
+    else:
+        print("Tie")
+        return
+
+    # not only one -- Tie
+    if milks.count(sec) > 1:
+        print("Tie")
+        return
+
+    # got the second
+    for i in log.keys():
+        if log[i] == sec:
+            print(i)
+
+find_sec()
+
+
+
+
+
+
