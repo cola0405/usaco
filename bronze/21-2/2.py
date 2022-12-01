66a685c72e9900a09b9940f54394c81f892d87fd
cola0405 <1020151695@qq.com>
2022-12-01 11:00:51 +0800

    Comfortable Cows

--- bronze/21-2/2.py
+++ bronze/21-2/2.py
@@ -1,8 +1,13 @@
-# exactly 3
+# exactly 3，周围邻接的有三个
+# “T”型，中间交界点就是comfortable
+# 想抱团但是不想被围住，谓之中间交界点就是comfortable
+
 # 新加入的牛可能会导致之前舒服的牛变得不舒服
+# 也可能会让它们变得舒服
 
+# 便利的同时检查可能会影响到的点
 
-# 一对一对地破坏
+# 2832ms
 
 border = 1000
 COMFORTABLE = 2
@@ -12,50 +17,58 @@ pasture = []
 for i in range(1001):
     pasture.append([0]*1001)
 
-def up(x,y):
-    if x-1 >= 0 and pasture[x-1][y] == 1:
-        return 1
-    elif x-1 >= 0 and pasture[x-1][y] == COMFORTABLE:
+def up(x, y):
+    if x-1 >= 0 and pasture[x-1][y] >= 1:
+        pasture[x-1][y] += 1
         global ans
-        ans -= 1
-        pasture[x - 1][y] = 1
+        if pasture[x-1][y] == 4:
+            ans += 1
+        if pasture[x-1][y] == 5:
+            ans -= 1
+        return 1
     return 0
 
-def down(x,y):
-    if x+1 <= border and pasture[x+1][y] == 1:
-        return 1
-    elif x+1 <= border and pasture[x+1][y] == COMFORTABLE:
+def down(x, y):
+    if x+1 <= border and pasture[x+1][y] >= 1:
+        pasture[x+1][y] += 1
         global ans
-        ans -= 1
-        pasture[x + 1][y] = 1
+        if pasture[x+1][y] == 4:
+            ans += 1
+        if pasture[x+1][y] == 5:
+            ans -= 1
+        return 1
     return 0
 
-def left(x,y):
-    if y-1 >= 0 and pasture[x][y-1] == 1:
-        return 1
-    elif y-1 >= 0 and pasture[x][y-1] == COMFORTABLE:
+def left(x, y):
+    if y-1 >= 0 and pasture[x][y-1] >= 1:
+        pasture[x][y-1] += 1
         global ans
-        ans -= 1
-        pasture[x][y - 1] = 1
+        if pasture[x][y-1] == 4:
+            ans += 1
+        if pasture[x][y-1] == 5:
+            ans -= 1
+        return 1
     return 0
 
-def right(x,y):
-    if y+1 <= border and pasture[x][y+1] == 1:
-        return 1
-    elif y+1 <= border and pasture[x][y+1] == COMFORTABLE:
+def right(x, y):
+    if y+1 <= border and pasture[x][y+1] >= 1:
+        pasture[x][y+1] += 1
         global ans
-        ans -= 1
-        pasture[x][y + 1] = 1
+        if pasture[x][y+1] == 4:
+            ans += 1
+        if pasture[x][y+1] == 5:
+            ans -= 1
+        return 1
     return 0
 
 
 ans = 0
 for i in range(n):
-    x,y = list(map(int, input().split()))
+    x, y = list(map(int, input().split()))
     pasture[x][y] = 1
     count = up(x, y) + down(x, y) + left(x, y) + right(x, y)
-    if count == 3:
-        pasture[x][y] = COMFORTABLE
+    pasture[x][y] += count
+    if count == 4:
         ans += 1
     print(ans)
 
