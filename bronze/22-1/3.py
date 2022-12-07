b3e56508ab99352bdfff2aece91febaf6165a673
cola0405 <1020151695@qq.com>
2022-12-07 10:44:18 +0800

    merge

--- bronze/22-1/3.py
+++ bronze/22-1/3.py
@@ -1,56 +1,41 @@
-# 我都没动！
-
+# 动了前面的i-2 就贪不了了
 
 round = int(input())
 
 for r in range(round):
     n = int(input())
-    lst = list(map(int, input().split()))
-    if len(lst) == 1:
+    levels = list(map(int, input().split()))
+    if n == 1:
         print(0)
         continue
-    diff = [0]
-    for i in range(1, len(lst)):
-        diff.append(lst[i] - lst[i-1])
-
-    tail = len(diff)-1
+    diff = [-1]
     ans = 0
-    fail = 0
-    while tail >= 0:
-        if diff[tail] < 0 and tail-2  >= 0:
-            if lst[tail-1] + diff[tail] >= 0 \
-                    and lst[tail-2] + diff[tail] >= 0:
-                ans += -2 * diff[tail]
-                lst[tail-1] += diff[tail]
-                lst[tail-2] += diff[tail]
-                diff[tail] = 0
-                tail -= 1
-                continue
+    for i in range(1, len(levels)):
+        diff.append(levels[i] - levels[i-1])
+    for i in range(len(diff))[1:]:
+        if diff[i] > 0:
+            if i+1 < len(levels) and levels[i+1] >= diff[i]:
+                gap = diff[i]
+                ans += gap
+                levels[i+1] -= gap
+                levels[i] -= gap
+                diff[i] = 0
+                if i+2 < len(levels):
+                    diff[i+2] += gap
             else:
-                fail = 1
+                print(-1)
                 break
-        if diff[tail] < 0 and tail-2  < 0:
-            fail = 1
-            break
-
-        if diff[tail] > 0 and tail+1 < len(lst):
-            if lst[tail+1] - diff[tail] >= 0\
-                    and lst[tail] - diff[tail] >= 0:
-                ans += 2 * diff[tail]
-                lst[tail] -= lst[tail]
-                lst[tail+1] -= lst[tail]
-                diff[tail] = 0
-                tail -= 1
-                continue
+        if diff[i] < 0:
+            if i-2 >= 0 and levels[i-2] >= abs(diff[i]):
+                gap = abs(diff[i])
+                ans += gap
+                levels[i-2] -= gap
+                levels[i-1] -= gap
+                diff[i] = 0
             else:
-                fail = 1
+                print(-1)
                 break
-        if diff[tail] > 0 and tail + 1 >= len(lst):
-            fail = 1
-            break
-        tail -= 1
-
-    if fail:
-        print(-1)
     else:
-        print(ans)
+        print(ans * 2)
+
+
