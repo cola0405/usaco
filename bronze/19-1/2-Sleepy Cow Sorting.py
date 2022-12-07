a0973cf8ab1646c53ef50c75af716c2feb9af696
cola0405 <1020151695@qq.com>
2022-12-07 18:52:56 +0800

    guess animal

--- bronze/19-1/2-Sleepy Cow Sorting.py
+++ bronze/19-1/2-Sleepy Cow Sorting.py
@@ -3,7 +3,6 @@
 
 
 import sys
-
 sys.stdin = open("sleepy.in", "r")
 sys.stdout = open("sleepy.out", "w")
 
---
+++ bronze/19-1/3.py
@@ -0,0 +1,40 @@
+# 枚举不出来的
+# 因为没办法通过遍历去进行淘汰
+
+# 题目看似是找正确的提问序列
+# 其实可以把问题进行转换
+# 找的其实是一对，最相似的一对1
+
+# 集合intersection
+
+
+import sys
+sys.stdin = open("guess.in", "r")
+sys.stdout = open("guess.out", "w")
+
+count = dict()
+animal_chas = dict()
+n = int(input())
+
+for i in range(n):
+    line = input().split()
+    animal = line[0]
+    chas = line[2:]
+    animal_chas[animal] = set(chas)
+
+count = 0
+pair = ()
+for i in animal_chas:
+    for j in animal_chas:
+        if i == j:
+            continue
+        intersect_num = len(animal_chas[i].intersection(animal_chas[j]))
+        count = max(count, intersect_num)
+
+print(count+1)
+
+
+
+
+
+
