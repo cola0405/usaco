971b2e7862b26a3a8cb8e3ee57c893fd75034bb2
cola0405 <1020151695@qq.com>
2022-12-26 17:59:07 +0800

    22-12-1 refactor

--- bronze/22-12/1.py
+++ bronze/22-12/1.py
@@ -2,17 +2,12 @@ n = int(input())
 c = list(map(int, input().split()))
 
 c.sort()
-already = set()
 
 ans = (0,0)
 for i in range(n):
     fee = c[i]
-    if fee in already:
-        continue
-    already.add(fee)
     value = fee * (n - i)
     if value > ans[0]:
         ans = (value, fee)
-
 
 print(ans[0], ans[1])
