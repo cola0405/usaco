85c19fe1f1acd5f19d326c79f9997d728e8ddef8
cola0405 <1020151695@qq.com>
2023-01-16 14:26:54 +0800

    15-12 refactor

--- bronze/15-12/3-p100.py
+++ bronze/15-12/3-p100.py
@@ -1,14 +1,17 @@
+# 信息点①
 # exactly one of them has gone bad
 # 只有一瓶奶出问题了
 # 但注意，我们不一定能准确地把那一瓶奶找出来
 # 尽可能排除，剩下地都当作有毒的
 
+# 信息点②
+# because they drank the bad milk at some strictly earlier point in time.
+# 不取等号
+
 # 排除的方法：
 # 把每个生病的人喝的可能有毒的奶找出来
 # 有毒的那一瓶一定在小集合里
-# 小集合外的一定是好牛奶
-# 所以利用n个小集合与大集合做差，尽可能排除
-
+# 则做交集来排除
 
 
 PERSON = 0
@@ -26,36 +29,24 @@ person_type_time = []
 for i in range(d):
     person_type_time.append(list(map(int, input().split())))
 
-person_sickTime = []
+# resume that all milk are bad, then shrink it
+bad_milk = set(range(1,m+1))
+
 for i in range(s):
-    person_sickTime.append(list(map(int, input().split())))
+    sick_people, sick_time = map(int, input().split())
 
-# initialize the set
-person_milk = {}
-for i in range(1, n+1):
-    person_milk[i] = set()
-
-# get possible bad milk
-possible_bad_milk = set()
-for s in person_sickTime:
-    for record in person_type_time:
-        if record[PERSON] == s[PERSON] \
-                and record[TIME] < s[SICK_TIME]:
-            possible_bad_milk.add(record[TYPE])
-            person_milk[record[PERSON]].add(record[TYPE])
-
-# remove the good milk
-for i in person_milk.keys():
-    if len(person_milk[i]) > 0:
-        good_milk = possible_bad_milk.difference(person_milk[i])
-        possible_bad_milk -= good_milk
+    # shrink bad milk
+    psb_bad_milk = set()
+    for people, milk_type, time in person_type_time:
+        if people == sick_people and time<sick_time:
+            psb_bad_milk.add(milk_type)
+    bad_milk = bad_milk.intersection(psb_bad_milk)
 
 # count the people
 possible_sick_person = set()
-for record in person_type_time:
-    if record[TYPE] in possible_bad_milk:
-        possible_sick_person.add(record[PERSON])
-
+for person, milk_type, time in person_type_time:
+    if milk_type in bad_milk:
+        possible_sick_person.add(person)
 
 print(len(possible_sick_person))
 
