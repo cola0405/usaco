a50b2d675600028372b10521152b5d92c8d042c9
cola0405 <1020151695@qq.com>
2023-03-17 22:49:10 +0800

    cow routing

---
+++ bronze/15-1/1.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.stdin = open('cowroute.in', 'r')
+sys.stdout = open('cowroute.out', 'w')
+
+
+A,B,N = map(int, input().split())
+
+ans = float('inf')
+for _ in range(N):
+    cost, city_num = map(int, input().split())
+    cities = list(map(int, input().split()))
+
+    if (A in cities and B in cities)\
+            and (cities.index(A) < cities.index(B)):
+        ans = min(cost,ans)
+
+if ans == float('inf'):
+    print(-1)
+else:
+    print(ans)
+
+
