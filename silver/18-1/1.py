0509f88ee4c259e8eba515aaf4762b30d60dcdb5
cola <52058772+cola0405@users.noreply.github.com>
2023-07-08 17:17:33 +0800

    update

--- silver/18-1/1.py
+++ silver/18-1/1.py
@@ -1,13 +1,11 @@
-# 按start排序
-# start point往前到last就是独占的
-# 再结合括号匹配
+# 把每对点拆出来，按先后顺序排列
+# 根据当前点是start还是end进行贪心统计sum和min_own
 
 import sys
 sys.stdin = open("lifeguards.in", "r")
 sys.stdout = open("lifeguards.out", "w")
 
 n = int(input())
-
 points = []
 for i in range(n):
     b,e = map(int, input().split())
@@ -29,17 +27,17 @@ s = dict()
 own = [0]*n
 for r in points:
     p, pair_id = r
+    gap = p-last
     if len(s) == 1:
-        # dict first item
+        # do have start
         for k in s:
-            own[k] += p - last
-            break
-    if len(s) > 0:
-        total += p - last
-    if id in s:
-        s.pop(pair_id)
-    else:
-        s[pair_id] = None
+            own[k] += gap
+    if len(s) > 0:  # 贪心积累total 只有左边有待匹配的start则都可累加
+        total += gap
+    if pair_id in s:  # current is the end
+        s.pop(pair_id)  # 弹出待匹配队列(不是queue)
+    else:   # current is the start
+        s[pair_id] = None  # 加入匹配队列
     last = p
 
 print(total - min(own))
