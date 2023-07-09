ef60bf742358b177c65953998c7a066d34763948
cola <52058772+cola0405@users.noreply.github.com>
2023-07-09 10:10:20 +0800

    update

--- silver/18-1/2_1.py
+++ silver/18-1/2_1.py
@@ -6,70 +6,47 @@ import sys
 sys.stdin = open("rental.in", "r")
 sys.stdout = open("rental.out", "w")
 
+def by_price(item):
+    return item[1]
+
+# 根据当前i的produce，阶梯看能卖多少钱
+def get_profit():
+    available = produce[i]
+    res = 0
+    global j
+    while available > 0 and j<M:
+        if available >= sell[j][0]:
+            res += sell[j][0]*sell[j][1]
+            available -= sell[j][0]
+            sell[j][0] = 0
+            j += 1
+        else:
+            res += available*sell[j][1]
+            sell[j][0] -= available
+            available = 0
+    return res
 
 N,M,R = map(int, input().split())
-
-# read produce
 produce = []
 for i in range(N):
     milk = int(input())
     produce.append(milk)
 produce.sort(reverse=True)
 
-# read sell
 sell = []
 for i in range(M):
     amount, price = map(int, input().split())
-    sell.append((amount, price))
-
-def by_price(item):
-    return item[1]
+    sell.append([amount, price])
 sell.sort(key=by_price, reverse=True)
 
-# 根据produce，阶梯看能卖多少钱
-def milks_money(total):
-    money = 0
-    i = 0
-    while total > 0:
-        if total - sell[i][0] > 0:
-            money += sell[i][0] * sell[i][1]
-            i += 1
-        else:
-            money += total * sell[i][1]
-        total -= sell[i][0]
-    return money
-
-
-
-
-# build prefix sum of selling
-# 贪心法+双指针，看前n头牛的奶能卖多少钱
 p_sell = [0]
 i = 0
 j = 0
-remain = sell[j][0]
-price = sell[j][1]
-while i < N and j < M:
-    money = 0
-    milk = produce[i]
-    while milk > 0 and j < M:
-        if milk > remain:
-            milk -= remain
-            money += remain * price
-            j += 1
-            if j == M:
-                break
-            remain = sell[j][0]
-            price = sell[j][1]
-        else:
-            money += milk * price
-            remain -= milk
-            milk = 0
-    p_sell.append(p_sell[-1] + money)
-    i += 1
-# 后置处理
+
+# 商店都买完后，能够自洽，不需要后置处理
 while i < N:
-    p_sell.append(p_sell[-1])
+    profit = get_profit()
+    p_sell.append(p_sell[-1] + profit)
     i += 1
 
 # read rent
@@ -79,14 +56,12 @@ for i in range(R):
     rent.append(price)
 rent.sort(reverse=True)
 
-# build prefix sum of renting
 # 出租n头牛分别能赚多少钱
 # 肯定是优先出租产奶量少的牛
 p_rent = [0]
 for rent_num in range(1, R+1):
     money = p_rent[-1] + rent[rent_num-1]
     p_rent.append(money)
-
 
 max_money = 0
 for sell_num in range(N+1):
