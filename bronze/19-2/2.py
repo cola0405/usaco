fb6cc7f332e395ac8ec3cac5f06ced4ad7d5526d
cola <1020151695@qq.com>
2024-01-03 15:25:38 +0800

    The Great Revegetation

--- bronze/19-2/2.py
+++ bronze/19-2/2.py
@@ -1,33 +1,33 @@
+# 贪心
 # 牛喜欢两个牧场，为了让他们吃的种类丰富，要求这两个牧场的牧草不同
 # 给出n对不同，要你给一个方案
 # 答案要求最小，则从1开始考虑给
 
-# 总的思路是根据n对不同，筛除不可用的，在可用的种里选最小的
-# 注意str的转换
+# 不与之前的牧场矛盾，然后选当前可用的最小的seed
+# 题目说的 "no pasture is a favorite of more than 3 cows."
+# 其实意味着cur之前最多3个种子没法用 -- 肯定会剩下一个能用的
 
-
+from collections import defaultdict
 import sys
 sys.stdin = open("revegetate.in", "r")
 sys.stdout = open("revegetate.out", "w")
 
 n, m = map(int, input().split())
-no_repeat = {}
-for i in range(1, n+1):
-    no_repeat[i] = []
+diff = defaultdict(list)    # 记录不同的pair
 
 for i in range(m):
     a, b = map(int, input().split())
-    no_repeat[a].append(b)
-    no_repeat[b].append(a)
+    if a < b:
+        diff[b].append(a)  # 只记录其之前的
+    else:
+        diff[a].append(b)
 
-ans = [0]
+grass = ['']   # '' 占位
 for pasture in range(1, n+1):
     seeds = ['1', '2', '3', '4']
-    for j in no_repeat[pasture]:
-        if j > pasture:
-            continue
-        if str(ans[j]) in seeds:
-            seeds.remove(ans[j])
-    ans.append(seeds[0])
+    for p in diff[pasture]:
+        if grass[p] in seeds:
+            seeds.remove(grass[p])  # 筛选掉不可用的 -- 与diff pair 矛盾的
+    grass.append(seeds[0])  # 选可用种子中最小的
 
-print(''.join(ans[1:]))
+print(''.join(grass[1:]))
