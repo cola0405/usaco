07c1cd1e08e58af74f560ce724b486483d131e19
cola <1020151695@qq.com>
2024-05-31 18:38:08 +0800

    update

--- bronze/18-1/1.py
+++ bronze/18-1/1.py
@@ -1,21 +1,6 @@
 # 熟练运用交集的几何知识
 
-import sys
-sys.stdin = open('billboard.in', 'r')
-sys.stdout = open('billboard.out', 'w')
-
-ax,ay,bx,by = map(int, input().split())
-cx,cy,dx,dy = map(int, input().split())
-
-min_top = min(by,dy)
-min_right = min(bx, dx)
-
-max_bottom = max(ay,cy)
-max_left = max(ax,cx)
-
-b_area = (by-ay) * (bx-ax)
-ans = b_area
-
+# consider of this situation
 #    ******
 #    *    *
 # *************
@@ -23,17 +8,23 @@ ans = b_area
 # *************
 #    *    *
 #    ******
-# 仍然需要覆盖整个板
-if (min_top < by and max_bottom > ay) \
-            or (min_right < bx and max_left > ax):
-    pass
 
-# 相交
-elif min_top > max_bottom and min_right > max_left:
-    dy = min_top - max_bottom
-    dx = min_right - max_left
-    # 需要减去相交部分
-    if dy == (by-ay) or dx == (bx-ax):
-        ans -= dx*dy
+import sys
+sys.stdin = open('billboard.in', 'r')
+sys.stdout = open('billboard.out', 'w')
+ax,ay,bx,by = map(int, input().split())
+cx,cy,dx,dy = map(int, input().split())
 
+min_top = min(by,dy)
+max_bottom = max(ay,cy)
+min_right = min(bx, dx)
+max_left = max(ax,cx)
+ans = (by-ay) * (bx-ax)
+
+# the overlap rectangle is cover the upper part or lower part
+if (min_right, min_top) == (bx, by) or (max_left, max_bottom) == (ax, ay):
+    t_height = min_top - max_bottom
+    t_width = min_right - max_left
+    if t_height == (by-ay) or t_width == (bx-ax):   # check valid rectangle
+        ans -= t_height * t_width
 print(ans)
